5693b6fa904cb4666ac4680532b2d9babbb58e8b
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-07-11 20:27:40 +0200

    Add Svelte integration and PostCSS configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
@@ -10,4 +12,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [svelte()],
 });
